9b34ea49ba74bb232c1e09701efc882b871cd489
Clara Guo <guoguo.clara@gmail.com>
2021-02-26 03:22:58 -0500

    Fixed error in per_corner_normals.cpp

--- meshes/src/per_corner_normals.cpp
+++ meshes/src/per_corner_normals.cpp
@@ -3,9 +3,7 @@
 // Hint:
 #include "vertex_triangle_adjacency.h"
 #include <iostream>
-
 #include <math.h>
-#include "per_vertex_normals.h"
 
 void per_corner_normals(
   const Eigen::MatrixXd & V,
@@ -24,20 +22,21 @@ void per_corner_normals(
     normal_weighted_sum = Eigen::RowVector3d(0, 0, 0);
     general_norm = triangle_area_normal(
       V.row(F(i, 0)), V.row(F(i, 1)), V.row(F(i, 2)));
+    for (int j = 0; j < F.cols(); ++j){
+      for (auto & face : VF[F(i, j)]) {
+        curr_norm = triangle_area_normal(
+          V.row(F(face, 0)), V.row(F(face, 1)), V.row(F(face, 2)));
 
-    for (auto & face : VF[i]) {
-      curr_norm = triangle_area_normal(
-        V.row(F(face, 0)), V.row(F(face, 1)), V.row(F(face, 2)));
-
-      // Compute the difference between curr_norm (unit vector) and normal_weighted_sum
-      double diff_angle = acos(abs(curr_norm.normalized().dot(general_norm.normalized())));
-      // Convert from radians to degree
-      diff_angle = diff_angle*180/M_PI;
-      // Use curr_norm, if diff_angle is larger than threshold
-      if(diff_angle > corner_threshold) {
-        normal_weighted_sum += curr_norm;
+        // Compute the difference between curr_norm (unit vector) and normal_weighted_sum
+        double diff_angle = acos(abs(curr_norm.normalized().dot(general_norm.normalized())));
+        // Convert from radians to degree
+        diff_angle = diff_angle*180/M_PI;
+        // Use curr_norm, if diff_angle is larger than threshold
+        if(diff_angle > corner_threshold) {
+          normal_weighted_sum += curr_norm;
+        }
       }
-      N.row(i*3 + face) = normal_weighted_sum.normalized();
+      N.row(i*3 + j) = normal_weighted_sum.normalized();
     }
   }
   ////////////////////////////////////////////////////////////////////////////
