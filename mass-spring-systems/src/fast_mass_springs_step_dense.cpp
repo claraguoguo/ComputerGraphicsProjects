6abd0494a1d1dfce14ac5bf37a3b9e5333a8e6e2
Clara Guo <guoguo.clara@gmail.com>
2021-03-22 18:40:03 -0400

    Fixed an error with penalty term

--- mass-spring-systems/src/fast_mass_springs_step_dense.cpp
+++ mass-spring-systems/src/fast_mass_springs_step_dense.cpp
@@ -21,11 +21,11 @@ void fast_mass_springs_step_dense(
   // Replace with your code
   double penalty_weight = 1e10;
   Eigen::MatrixXd d, y, b_penalty, updated_p;
-  
+
   updated_p = Ucur;
   for(int iter = 0; iter < 50; iter++)
   {
-    d = Eigen::MatrixXd::Zero(r.size(), 1);
+    d = Eigen::MatrixXd::Zero(r.size(), 3);
     int i, j;
     for (int row = 0; row < r.size(); ++row) {
       i = E(row, 0);
@@ -34,7 +34,7 @@ void fast_mass_springs_step_dense(
     }
 
     y = (1.0/pow(delta_t, 2))*M*(2*Ucur - Uprev) + fext;
-    b_penalty = k*A.transpose()*d + y + penalty_weight*C.transpose()*C; 
+    b_penalty = k*A.transpose()*d + y + penalty_weight*C.transpose()*C*V; 
     updated_p = prefactorization.solve(b_penalty);
   }
 
